f8d7cc2efeef190a564c69a1cc540ed1812d48a0
Dan Gunter <dkgunter@lbl.gov>
2017-08-18 03:18:43 -0700

    python skeleton

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup
+
+config = {
+        'description': 'ALS Data',
+        'author': 'Dan Gunter',
+        'url': 'http://www.lbl.gov',
+        'download_url': 'http://www.lbl.gov/',
+        'author_email': 'dkgunter@lbl.gov',
+        'version': '0.0.1',
+        'install_requires': [],
+        'packages': ['alsdata'],
+        'scripts': [],
+        'name': 'ALSData'
+}
+
+setup(**config)
---
+++ tests/test_alsdata.py
@@ -0,0 +1,11 @@
+
+import alsdata
+
+def setup():
+    pass
+
+def teardown():
+    pass
+
+def test_basic():
+    pass
